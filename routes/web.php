83695c2b70c5cc95ac3f2855610baa8f071403bb
Esmillone Rosal <esmillone.rosal@gmail.com>
2025-06-15 20:50:39 +0800

    Part 2

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,5 @@ Route::middleware(['auth'])->group(function () {
     Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
 });
 
-echo "Hola";
 
 require __DIR__ . '/auth.php';
